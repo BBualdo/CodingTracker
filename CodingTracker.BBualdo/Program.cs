c59be1e40c660308de80b9efae119923c1150238
Sebastian <mrseb10@gmail.com>
2024-04-12 01:02:13 +0200

    Added Configuration File, DbContext and Creating Tables with Seeding

--- CodingTracker.BBualdo/Program.cs
+++ CodingTracker.BBualdo/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CodingTracker.Database;
+
+DbContext db = new DbContext();
---
+++ CodingTracker.Database/DbContext.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Configuration;
+
+namespace CodingTracker.Database;
+
+public class DbContext
+{
+  public string ConnectionString { get; set; } = ConfigurationManager.AppSettings.Get("ConnectionString")!;
+
+  public DbContext()
+  {
+    CreateTables();
+    SeedData();
+  }
+
+  private void CreateTables()
+  {
+    using (SqliteConnection connection = new SqliteConnection(ConnectionString))
+    {
+      connection.Open();
+
+      string createTablesSql = @"CREATE TABLE IF NOT EXISTS sessions(
+                              session_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                              start_date TEXT,
+                              end_date TEXT,
+                              duration INT);
+                              CREATE TABLE IF NOT EXISTS goals(
+                              goal_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                              start_date TEXT,
+                              target_duration TEXT)";
+
+      using (SqliteCommand createCommand = new SqliteCommand(createTablesSql, connection))
+      {
+        createCommand.ExecuteNonQuery();
+      }
+    }
+  }
+
+  private void SeedData()
+  {
+    Console.WriteLine("Loading...");
+
+    using (SqliteConnection connection = new SqliteConnection(ConnectionString))
+    {
+      connection.Open();
+
+      string countRecordsSql = "SELECT COUNT(*) FROM sessions";
+
+      using (SqliteCommand countCommand = new SqliteCommand(countRecordsSql, connection))
+      {
+        int recordsNumber = Convert.ToInt32(countCommand.ExecuteScalar());
+
+        Random random = new Random();
+
+        if (recordsNumber == 0)
+        {
+          for (int i = 0; i < 10; i++)
+          {
+            DateTime startDateTime = DateTime.Now.AddDays(-random.Next(0, 365)).AddHours(random.Next(0, 24)).AddMinutes(random.Next(0, 60));
+            DateTime endDateTime = startDateTime.AddHours(random.Next(0, 7)).AddMinutes(random.Next(0, 60));
+
+            TimeSpan durationTimeSpan = endDateTime - startDateTime;
+
+            string startDate = startDateTime.ToString("dd-MM-yy HH:mm");
+            string endDate = endDateTime.ToString("dd-MM-yy HH:mm");
+            int duration = Convert.ToInt32(durationTimeSpan.TotalMinutes);
+
+            string insertSql = $"INSERT INTO sessions(start_date, end_date, duration) VALUES('{startDate}', '{endDate}', {duration})";
+
+            using (SqliteCommand insertCommand = new SqliteCommand(insertSql, connection))
+            {
+              insertCommand.ExecuteNonQuery();
+            }
+          }
+        }
+      }
+    }
+
+    Console.Clear();
+  }
+}
